d4e77ee7b845e970abc6c00d4f9639dcf272b91f
SarahNarsham <91451904+SarahNarsham@users.noreply.github.com>
2021-11-23 09:59:47 +0800

    javaScript

---
+++ script.js
@@ -0,0 +1,26 @@
+function func() {
+    alert("Page is loaded.");
+}
+
+function fokus(x) {
+    x.style.background = "yellow";
+}
+
+function blurx(y) {
+    y.style.background = "magenta";
+}
+
+function funct() {
+    alert("I'm an alert box!");
+}
+
+function fun() {
+    confirm("I;m a confirm box!");
+}
+
+function func() {
+    var person = prompt("Please enter your name");
+    if (person != null) {
+        document.getElementById("demo").innerHTML = "Hello " + person;
+    }
+}
